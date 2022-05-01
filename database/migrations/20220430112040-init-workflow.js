978dbc1d9457cd8f820611dc7fa9092c39cdaf44
rhythm022 <rhythm22@yeah.net>
2022-05-01 09:47:35 +0800

    补全 model 定义

---
+++ database/migrations/20220430112040-init-workflow.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable('workflow', {
+      id: { type: INTEGER, primaryKey: true, },
+      name: STRING(30),
+      workflow_name: STRING(30),
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+  down: async () => {},
+};
